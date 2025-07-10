1e7c111b8c7484b75919a23dc25853b505697349
Hariom Gupta <hariomgupta18082003@gmail.com>
2025-07-11 00:02:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Smooth scroll for internal links (optional enhancement)
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    if (target) {
-      target.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-});
+
